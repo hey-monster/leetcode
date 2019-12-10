ed33bb29353ee64a384b9cad98268e2c2d096a03
hey-monster <50080058+hey-monster@users.noreply.github.com>
2019-12-10 19:48:54 +0800

    Update 0002.Add Two Numbers.cpp

--- code/0002.Add Two Numbers.cpp
+++ code/0002.Add Two Numbers.cpp
@@ -33,3 +33,25 @@ public:
         return lTmp->next;
     }
 };
+
+
+/*使用下面的写法耗时减少接近50%*/
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(-1), *cur = dummy;
+        int carry = 0;
+        while (l1 || l2) {
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        if (carry) cur->next = new ListNode(1);
+        return dummy->next;
+    }
+};
